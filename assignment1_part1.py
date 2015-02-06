fbde563fbcae348f33dd02d71857ae3d03e9c21d
unknown <rdeleston@NYSD19.eci.corp>
2015-02-05 20:29:41 -0500

    Complete assignment Part 1

---
+++ assignment1_part1.py
@@ -0,0 +1,23 @@
+def listDivide(numbers, divide=2):
+  even = 0
+  for i in numbers:
+    if i % divide == 0:
+      even +=1
+  return even
+
+def ListDivideException():
+  raise "There is an error!"
+
+def testListDivide():
+  if listDivide([1,2,3,4,5]) == 2:
+    pass
+  elif listDivide([2,4,6,8,10]) == 5:
+    pass
+  elif listDivide([30,54,63,98,100], divide=10):
+    pass
+  elif listDivide([]) == 0:
+    pass
+  else:
+    ListDivideException()
+
+  
